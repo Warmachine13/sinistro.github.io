db50a6f133aff3e06891152c7baf81fa94ad87c7
Warmachine13 <lucaspereiradesouzat@gmail.com>
2019-09-12 00:14:52 -0300

    Updates

---
+++ precache-manifest.ab5d40444572779e31868090ac0b138f.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b286f69ac0808f6931e7cb28ad8f667",
+    "url": "/sinistro.github.io/index.html"
+  },
+  {
+    "revision": "022244e473d074d6daec",
+    "url": "/sinistro.github.io/static/css/main.49ff1521.chunk.css"
+  },
+  {
+    "revision": "227bc14fee36f259d29a",
+    "url": "/sinistro.github.io/static/js/2.0dae59ca.chunk.js"
+  },
+  {
+    "revision": "022244e473d074d6daec",
+    "url": "/sinistro.github.io/static/js/main.163ae64e.chunk.js"
+  },
+  {
+    "revision": "77fe5125daff2e509862",
+    "url": "/sinistro.github.io/static/js/runtime~main.ba3b7612.js"
+  },
+  {
+    "revision": "57f20fde4729ab8a0f6385710519710a",
+    "url": "/sinistro.github.io/static/media/Biblia1.57f20fde.png"
+  },
+  {
+    "revision": "25f4a51bbc0eebe8f291214c6a3d8286",
+    "url": "/sinistro.github.io/static/media/Biblia2.25f4a51b.png"
+  },
+  {
+    "revision": "a7a9f4c1cbced15807f386629e016e60",
+    "url": "/sinistro.github.io/static/media/Biblia3.a7a9f4c1.png"
+  },
+  {
+    "revision": "09e62e6cad8283747bf5be82aa3c8f1c",
+    "url": "/sinistro.github.io/static/media/CameraCompleta1.09e62e6c.png"
+  },
+  {
+    "revision": "1ab42edfcc6da3f28a3d079d04fccb9f",
+    "url": "/sinistro.github.io/static/media/CameraCompleta2.1ab42edf.png"
+  },
+  {
+    "revision": "1dcd03982b29c65046387d06f6d9f4b9",
+    "url": "/sinistro.github.io/static/media/CameraCompleta3.1dcd0398.png"
+  },
+  {
+    "revision": "e759880748504b03f562cf4eda8a31e7",
+    "url": "/sinistro.github.io/static/media/CameraCompleta4.e7598807.png"
+  },
+  {
+    "revision": "c99640ed80b9186af8453bc5022bf449",
+    "url": "/sinistro.github.io/static/media/CameraCompleta5.c99640ed.png"
+  },
+  {
+    "revision": "d89e073bde32fe7ed4662f7eae360a47",
+    "url": "/sinistro.github.io/static/media/CameraCompleta6.d89e073b.png"
+  },
+  {
+    "revision": "071c73007f774a8a00bc16f806345aa5",
+    "url": "/sinistro.github.io/static/media/ContatosBlack1.071c7300.png"
+  },
+  {
+    "revision": "499a8bba9da6710f1b2ac4e105322cfe",
+    "url": "/sinistro.github.io/static/media/ContatosBlack2.499a8bba.png"
+  },
+  {
+    "revision": "6b1144566e98b5b98793a229e691acfe",
+    "url": "/sinistro.github.io/static/media/ContatosBlack3.6b114456.png"
+  },
+  {
+    "revision": "1529e3a15a3369ae96ac42a61ab57d19",
+    "url": "/sinistro.github.io/static/media/ContatosBlack4.1529e3a1.png"
+  },
+  {
+    "revision": "1997b04ad67c3f1e3ae7d683c2c7c16a",
+    "url": "/sinistro.github.io/static/media/ControleDiabetes1.1997b04a.png"
+  },
+  {
+    "revision": "fc2bc807b50dacb2c8fb05033430c445",
+    "url": "/sinistro.github.io/static/media/ControleDiabetes2.fc2bc807.png"
+  },
+  {
+    "revision": "01ed73561e40daad054d22b5b20c9141",
+    "url": "/sinistro.github.io/static/media/ControleDiabetes3.01ed7356.png"
+  },
+  {
+    "revision": "1ae9946fd0af2e44c306eb487268d675",
+    "url": "/sinistro.github.io/static/media/ControleDiabetes4.1ae9946f.png"
+  },
+  {
+    "revision": "b09fc4e1a26cedda2e9a0ddd4956bc0f",
+    "url": "/sinistro.github.io/static/media/ControleDiabetes5.b09fc4e1.png"
+  },
+  {
+    "revision": "4cce945aaee8947f36cf4cf5c6e8a8c1",
+    "url": "/sinistro.github.io/static/media/ControleDiabetes6.4cce945a.png"
+  },
+  {
+    "revision": "7b05e56d30d1cc07119182cc4b1ed4f7",
+    "url": "/sinistro.github.io/static/media/ControleDiabetes7.7b05e56d.png"
+  },
+  {
+    "revision": "d8b5f469ea255136a7748c235eb7a4d2",
+    "url": "/sinistro.github.io/static/media/ControleDiabetes8.d8b5f469.png"
+  },
+  {
+    "revision": "7867e99a4d26618e80a3cd0a230aaf5d",
+    "url": "/sinistro.github.io/static/media/DesenhosTop1.7867e99a.png"
+  },
+  {
+    "revision": "01e0d50cb05ae64de33092882eea839c",
+    "url": "/sinistro.github.io/static/media/DesenhosTop2.01e0d50c.png"
+  },
+  {
+    "revision": "6b71fe4190d651f0a7ed52230cb22597",
+    "url": "/sinistro.github.io/static/media/DesenhosTop3.6b71fe41.png"
+  },
+  {
+    "revision": "51e5a5a35cd4b75a58400be24ab095f9",
+    "url": "/sinistro.github.io/static/media/DesenhosTop4.51e5a5a3.png"
+  },
+  {
+    "revision": "5fb4f26f280b87b0e6a89a9a93c6a93e",
+    "url": "/sinistro.github.io/static/media/Evil-512.5fb4f26f.png"
+  },
+  {
+    "revision": "3de0b56dd1fd11d9fb60da998a53246a",
+    "url": "/sinistro.github.io/static/media/Evilr-512.3de0b56d.png"
+  },
+  {
+    "revision": "ba74ac43593795da12422b61cef65c03",
+    "url": "/sinistro.github.io/static/media/FotosparaTexto1.ba74ac43.png"
+  },
+  {
+    "revision": "edaa3e65eea249c2acf555d9ffcbd0ed",
+    "url": "/sinistro.github.io/static/media/FotosparaTexto2.edaa3e65.png"
+  },
+  {
+    "revision": "f7fd61d8196b740e33eed6471c66a487",
+    "url": "/sinistro.github.io/static/media/FotosparaTexto3.f7fd61d8.png"
+  },
+  {
+    "revision": "c6526ea57df0e4154863cfb00ed403ef",
+    "url": "/sinistro.github.io/static/media/FotosparaTexto4.c6526ea5.png"
+  },
+  {
+    "revision": "5dd3ec444241f26829e8caabfd7f4d4a",
+    "url": "/sinistro.github.io/static/media/NovoCartaoDigital1.5dd3ec44.png"
+  },
+  {
+    "revision": "a523dae853f664bfab44f50c7add2f66",
+    "url": "/sinistro.github.io/static/media/NovoCartaoDigital2.a523dae8.png"
+  },
+  {
+    "revision": "2a23ccbc48f9b8630c597594bd4fc69b",
+    "url": "/sinistro.github.io/static/media/Technology-wallpaper4.2a23ccbc.jpg"
+  },
+  {
+    "revision": "ed4359055f0e2417c828eac2ec94d883",
+    "url": "/sinistro.github.io/static/media/background1.ed435905.jpg"
+  },
+  {
+    "revision": "15b419bf2fcf737603460c7bcab3a9e7",
+    "url": "/sinistro.github.io/static/media/background2.15b419bf.jpeg"
+  },
+  {
+    "revision": "0463007aba2dbf3afe781c660ca9b266",
+    "url": "/sinistro.github.io/static/media/background3.0463007a.jpeg"
+  },
+  {
+    "revision": "7efa2e27d87e50e9350e9a4ba81059eb",
+    "url": "/sinistro.github.io/static/media/background4.7efa2e27.jpg"
+  },
+  {
+    "revision": "a9eef0eb46882908df4b426c09db1d33",
+    "url": "/sinistro.github.io/static/media/background5.a9eef0eb.jpg"
+  },
+  {
+    "revision": "9e8e14644e117254275e2e7e69de4ac2",
+    "url": "/sinistro.github.io/static/media/background6.9e8e1464.jpg"
+  },
+  {
+    "revision": "c6a6de7e94abdb3efa1d9c4d3e0680cd",
+    "url": "/sinistro.github.io/static/media/cep1.c6a6de7e.png"
+  },
+  {
+    "revision": "9a296fd84a1b5fa5d62216e420c40fda",
+    "url": "/sinistro.github.io/static/media/flashlight1.9a296fd8.png"
+  },
+  {
+    "revision": "22e77f09892a710257a5760bfc566115",
+    "url": "/sinistro.github.io/static/media/flashlight2.22e77f09.png"
+  },
+  {
+    "revision": "07161a973dec8b43b78dfcb97c056f94",
+    "url": "/sinistro.github.io/static/media/fotoperfil.07161a97.jpg"
+  },
+  {
+    "revision": "b882d9748b5b2f75a372a616f40c3f7d",
+    "url": "/sinistro.github.io/static/media/geoClima1.b882d974.png"
+  },
+  {
+    "revision": "eb3a4989efce8022bd5740ecc7c88c18",
+    "url": "/sinistro.github.io/static/media/geoClima2.eb3a4989.png"
+  },
+  {
+    "revision": "3e3a086163b1c7410e87f1d29bfd0e7b",
+    "url": "/sinistro.github.io/static/media/getip1.3e3a0861.png"
+  },
+  {
+    "revision": "6339a146481dfc2f3a50148b5c521971",
+    "url": "/sinistro.github.io/static/media/getip2.6339a146.png"
+  },
+  {
+    "revision": "550a998eca16b332f29864741c73a3dc",
+    "url": "/sinistro.github.io/static/media/musicblack1.550a998e.png"
+  },
+  {
+    "revision": "257bf03a60a1313f2ee2cae414d2f351",
+    "url": "/sinistro.github.io/static/media/musicblack2.257bf03a.png"
+  },
+  {
+    "revision": "e08f5f93fabcf944dbc3a03db019cd08",
+    "url": "/sinistro.github.io/static/media/musicblack3.e08f5f93.png"
+  },
+  {
+    "revision": "ff2141684bf6905278d9b48aaa2ecc4c",
+    "url": "/sinistro.github.io/static/media/musicblack4.ff214168.png"
+  },
+  {
+    "revision": "f149c602623811d3b467e858fd72f079",
+    "url": "/sinistro.github.io/static/media/reconhecimentoVoz1.f149c602.png"
+  },
+  {
+    "revision": "b74d2001212df85c2e38b45c43f67f65",
+    "url": "/sinistro.github.io/static/media/reconhecimentoVoz2.b74d2001.png"
+  },
+  {
+    "revision": "00ca0dc267e6aa3882fe6a6e7108237f",
+    "url": "/sinistro.github.io/static/media/thumb-350-317353.00ca0dc2.jpg"
+  },
+  {
+    "revision": "4bfa8546e6d5407cbd52e28424b52959",
+    "url": "/sinistro.github.io/static/media/thumb-350-388041.4bfa8546.jpg"
+  },
+  {
+    "revision": "fd649ce91a4f77e9191461cd56f969cf",
+    "url": "/sinistro.github.io/static/media/thumb-350-403441.gif.fd649ce9.png"
+  },
+  {
+    "revision": "47b14fe1e45a4b350d469b941c6b0dd6",
+    "url": "/sinistro.github.io/static/media/thumb-350-456498.47b14fe1.jpg"
+  },
+  {
+    "revision": "de5f8d4efc9a3d153b55a0024b043d80",
+    "url": "/sinistro.github.io/static/media/thumb-350-546191.de5f8d4e.jpg"
+  },
+  {
+    "revision": "433cdf15d04ce64b27aa911519861dc4",
+    "url": "/sinistro.github.io/static/media/thumb-350-648552.433cdf15.jpg"
+  },
+  {
+    "revision": "594b636729fccbdbaa69de963ff21d49",
+    "url": "/sinistro.github.io/static/media/thumb-350-9728.594b6367.jpg"
+  }
+]);
